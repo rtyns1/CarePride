1d9bf1df390ef671394d9a4c00f966c9caf2326f
Karel Martyns <martynskarel@gmail.com>
2026-07-13 23:49:29 +0300

    Initial commit: CarePride Education System source code

---
+++ CarePride.Desktop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarePride.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarePride.Domain/Enums/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarePride.Domain.Enums
+{
+    internal class Gender
+    {
+    }
+}
